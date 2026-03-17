87d2fe1900ac1671db838d13a02c207d32642d69
dovee <ty202500325@wmsu.edu.ph>
2026-03-17 13:07:49 +0800

    Add files via upload

---
+++ Assignment_002.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void analyzeGrades(int *matrix, int students, int subjects) {
+    int highest = *(matrix); 
+
+    cout << "\nGrade Matrix:\n";
+    for (int i = 0; i < students; i++) {
+        float sum = 0;
+
+        for (int j = 0; j < subjects; j++) {
+            int value = *(matrix + i * subjects + j);
+            cout << value << "\t";
+
+            sum += value;
+
+            if (value > highest) {
+                highest = value;
+            }
+        }
+
+        cout << " | Average: " << sum / subjects;
+        cout << endl;
+    }
+
+    cout << "\nHighest Grade: " << highest << endl;
+}
+
+int main() {
+    int students, subjects;
+
+    cout << "Enter number of students: ";
+    cin >> students;
+
+    cout << "Enter number of subjects: ";
+    cin >> subjects;
+
+    
+    int *grades = new int[students * subjects];
+
+    
+    cout << "\nEnter grades:\n";
+    for (int i = 0; i < students; i++) {
+        for (int j = 0; j < subjects; j++) {
+            cout << "Student " << i + 1 << ", Subject " << j + 1 << ": ";
+            cin >> *(grades + i * subjects + j);
+        }
+    }
+
+    analyzeGrades(grades, students, subjects);
+
+    delete[] grades;
+
+    return 0;
+}
